5955765080650e6b710ef27e751a3d7b10829e23
Rafael Martinez <rafael.martinezcalvo@orange.fr>
2024-08-12 00:30:42 +0200

    update node UI

--- scenes/main/node/base/ui/node_ui.gd
+++ scenes/main/node/base/ui/node_ui.gd
@@ -1,14 +1,16 @@
 @tool
-extends NinePatchRect
+extends Panel
 
 
 @export_range(0, 360) var hue := 0.0
 
 
 func apply():
-	self.self_modulate = Color.from_hsv(hue/360, 15.0/100, 34.0/100, 1.0)
-	$Wrapper/Container/Button/Control2/NinePatchRect.self_modulate = Color.from_hsv(hue/360, 16.0/100, 28.0/100, 1.0)
-	$Wrapper/ProgressBar.tint_progress = Color.from_hsv(hue/360, 16.0/100, 28.0/100, 1.0)
+	# TODO: Fix this so that it is unique and not global
+	# self.get_theme_stylebox("panel", "Panel").bg_color = Color.from_hsv(hue/360, 15.0/100, 34.0/100, 1.0)
+	
+
+	$ProgressBar.tint_progress = Color.from_hsv(hue/360, 16.0/100, 28.0/100, 1.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
