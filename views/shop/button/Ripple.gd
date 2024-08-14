8d67e1ce028b80d50326abee2738f8d857eb104f
rafalou38 <rafael.martinezcalvo@orange.fr>
2024-08-14 18:32:34 +0200

    Cleanup ripple script

--- views/shop/button/Ripple.gd
+++ views/shop/button/Ripple.gd
@@ -5,12 +5,7 @@ extends Control
 
 # Function to start the ripple effect
 func _on_Button_pressed():
-	# var ripple = self.get_child(0)
-	# ripple.rect_scale = Vector2(1.0, 1.0)
-	# ripple.modulate.a = 1.0
-	print("Ripple")
 	self.visible = true
-	# Util.screen_to_world(get_viewport(), position)
 	self.global_position = self.get_global_mouse_position()
 
 	animation_player.play("RESET")
@@ -19,14 +14,7 @@ func _on_Button_pressed():
 func _end(_anim_name: StringName):
 	self.visible = false
 
-# Make sure to connect the Button's pressed signal to the _on_Button_pressed function.
 func _ready():
 	btn.connect("button_down", self._on_Button_pressed)
 	animation_player.connect("animation_finished", self._end)
 	animation_player.play("RESET")
-
-# func _process(_delta):
-# 	if btn.disabled:
-# 		modulate.a = 0.5
-# 	else:
-# 		modulate.a = 1.0
