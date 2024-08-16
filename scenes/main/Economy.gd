980175bb2c0ab5b2322a38585c70e31e4071f7b0
rafalou38 <rafael.martinezcalvo@orange.fr>
2024-08-16 18:43:17 +0200

    working money derivative

--- scenes/main/Economy.gd
+++ scenes/main/Economy.gd
@@ -14,14 +14,19 @@ static var research := {
 }
 
 
-static var _prev_money := 0
-static var _prev_money_time := 0
-static var _prev_dm := 0
+static var _prev_money := 0.0
+static var _prev_money_time := 0.0
+static var _dvm := 0.0
 static func var_money() -> float:
     var now = Time.get_unix_time_from_system()
-    if now - _prev_money_time >= 20:
-        _prev_dm = (money - _prev_money) / (now - _prev_money_time)
+
+    if _prev_money > money:
         _prev_money = money
         _prev_money_time = now
+    if _prev_money < money:
+        _dvm = min(lerpf(_dvm, (money - _prev_money) / (now - _prev_money_time), 0.8), _dvm * 1.8)
 
-    return max(0, (money - _prev_money) / (now - _prev_money_time))
+        _prev_money_time = now
+        _prev_money = money
+
+    return _dvm
